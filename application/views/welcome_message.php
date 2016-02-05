e75c79b2dd979c372e81bfb24a3d78084a4f96a3
AlineSieczko <alinesieczko@gmail.com>
2016-02-05 13:33:02 -0200

    alterando cor letra e fundo

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -12,22 +12,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	::-moz-selection { background-color: #E13300; color: white; }
 
 	body {
-		background-color: #fff;
+		background-color: #FFC9FF;
 		margin: 40px;
 		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
+		color: #000000;
 	}
 
 	a {
-		color: #003399;
+		color: #000000;
 		background-color: transparent;
 		font-weight: normal;
 	}
 
 	h1 {
-		color: #444;
+		color: #000000;
 		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
+		border-bottom: 1px solid #000000;
 		font-size: 19px;
 		font-weight: normal;
 		margin: 0 0 14px 0;
